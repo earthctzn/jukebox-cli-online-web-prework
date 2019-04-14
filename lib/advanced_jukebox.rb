4e938e461f8497139973656ca832f72af56b3c7e
Caleb Einarr <earthctzn1@gmail.com>
2019-04-14 14:55:48 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -54,22 +54,4 @@ def exit_jukebox
   puts "Goodbye" 
 end
 
-def run(my_songs)
-  #this method is the same as in jukebox.rb
-  help
-  loop.do
-    puts "Please enter a command:"
-    response = gets.chomp
-    if response == "list"
-      list(my_songs)
-    elsif response == "play"
-      play(my_songs)
-    elsif response == "help"
-      help
-    elsif response == "exit"
-      exit_jukebox
-    break
-    end
-  end
-end
-end
+
