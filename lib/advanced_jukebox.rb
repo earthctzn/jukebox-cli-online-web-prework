67447a5e5c38edd19491aa4044becb3309187718
Caleb Einarr <earthctzn1@gmail.com>
2019-04-14 13:31:09 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -12,7 +12,7 @@
  "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
  }
 
-def help
+def helps
  puts "I accept the following commands:
 - help : displays this help message
 - list : displays a list of songs you can play
