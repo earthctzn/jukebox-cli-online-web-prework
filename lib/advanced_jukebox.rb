b1d1e71736439e4f8b88855876ba6e9429596b3e
Caleb Einarr <earthctzn1@gmail.com>
2019-04-14 14:52:34 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -60,16 +60,16 @@ def run(my_songs)
   loop.do 
   puts "Please enter a command:"
   response = gets.chomp
-  if response == "list"
+    if response == "list"
     list(my_songs)
-  elsif response == "play"
+    elsif response == "play"
     play(my_songs)
-  elsif response == "help"
+    elsif response == "help"
     help
-  elsif response == "exit"
+    elsif response == "exit"
     exit_jukebox
     break
-  end
+    end
   end
 end
 end
