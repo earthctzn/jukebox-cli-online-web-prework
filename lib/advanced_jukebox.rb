9d643b50fcc979a526b4efecf6f2ab72e82b2ea1
Caleb Einarr <earthctzn1@gmail.com>
2019-04-14 13:29:45 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -13,10 +13,11 @@
  }
 
 def help
-  helps = {}
-  commands = {(?=.*help)(?=.*list)(?=.*play)(?=.*exit)}
-  commands.each do |command| 
-    helps
+ puts "I accept the following commands:"
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
   
 end
 
