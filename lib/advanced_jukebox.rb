d684b76b9bc8956365d7bd81824a252b2c404f8b
Caleb Einarr <earthctzn1@gmail.com>
2019-04-14 13:16:09 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -12,7 +12,7 @@
  "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
  }
 
-def help
+def helps
   commands = {"h" => "help", "l" => "list", "p" => "play", "e" => "exit"}
   commands.each {|i, command|puts command}
 end
